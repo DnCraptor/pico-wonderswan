257ddae653b4ae26522e5e72d60c8693f8d818be
Mike Vengerovich <mikeveng73@gmail.com>
2026-09-19 01:47:34 +0300

    + HDMI support

--- drivers/hdmi/hdmi.c
+++ drivers/hdmi/hdmi.c
@@ -188,7 +188,7 @@ static void __scratch_y("hdmi_driver") dma_handler_HDMI() {
             case GRAPHICSMODE_DEFAULT:
             case VGA_320x240x256: {
                 //заполняем пространство сверху и снизу графического буфера
-                if (y < graphics_buffer_shift_y || y > graphics_buffer_height) {
+                if (y < graphics_buffer_shift_y || y >= (graphics_buffer_shift_y + graphics_buffer_height)) {
                     memset(output_buffer, 255,SCREEN_WIDTH);
                     break;
                 }
@@ -199,13 +199,10 @@ static void __scratch_y("hdmi_driver") dma_handler_HDMI() {
                 output_buffer += graphics_buffer_shift_x;
 
                 //рисуем сам видеобуфер+пространство справа
-                input_buffer = &graphics_buffer[(y - graphics_buffer_shift_y) * (16+320+16)];
+                input_buffer = &graphics_buffer[(y - graphics_buffer_shift_y) * graphics_buffer_width];
 
                 const uint8_t* input_buffer_end = input_buffer + graphics_buffer_width;
 
-                if (graphics_buffer_width == 320) {
-                    input_buffer += 16;
-                }
                 if (graphics_buffer_shift_x < 0) input_buffer -= graphics_buffer_shift_x;
 
                 while (activ_buf_end > output_buffer) {
